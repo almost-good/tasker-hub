b081ef0e09b3b752637358a900ad6ad0f8c2ec1e
almost-good <marja.marinic@gmail.com>
2025-01-08 19:23:00 +0000

    add Task model

--- tasker/models.py
+++ tasker/models.py
@@ -1,3 +1,12 @@
+from email.mime import image
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasker_tasks')
+    is_completed = models.BooleanField(default=False)
+    date_updated = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
