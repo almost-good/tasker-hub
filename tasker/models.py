22bc978b336f16f0393a944187afc6053b451ab7
almost-good <marja.marinic@gmail.com>
2025-01-08 20:12:27 +0000

    add docstring to Task model

--- tasker/models.py
+++ tasker/models.py
@@ -4,6 +4,22 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+    '''
+    Stores a single task entry related to :model:`auth.User`.
+    
+    Fields:
+    - name: The name of the task.
+    - author: The user who created the task.
+    - is_completed: A boolean indicating if the task is completed.
+    - date_updated: The date and time when the task was last updated.
+    - likes: The number of likes the task has received.
+    
+    Constraints:
+    - Unique task name per author.
+    
+    Methods:
+    - __str__: String representation of the model.
+    '''
     name = models.CharField(max_length=200)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='tasker_tasks')
@@ -18,5 +34,6 @@ class Task(models.Model):
                 fields=['name', 'author'], name='unique_task_name_per_author')
         ]
     
+    # String representation of the model.
     def __str__(self):
         return f"TASK: {self.name} | {self.author}"
