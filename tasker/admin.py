1bb448db9ca26babdbf5b49ff543906eeb5e445c
almost-good <marja.marinic@gmail.com>
2025-01-09 21:35:27 +0000

    add filter and display options for Task model to admin panel

--- tasker/admin.py
+++ tasker/admin.py
@@ -3,11 +3,16 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Task, Subtask
 
 
-# Model registration for: Task.
 @admin.register(Task)
 class TaskAdmin(SummernoteModelAdmin):
-    list_display = ('name',)
+    '''
+    Model admin for :model:`tasker.Task`.
+    '''
     readonly_fields = ('slug',) 
+    list_display = (
+        'name', 'slug', 'author', 'date_updated', 'likes', 'is_completed')
+    list_filter = ('date_updated', 'is_completed')
+    search_fields = ['name', 'author__username', 'is_completed']
 
 
 # Model registration for: Subtask.
