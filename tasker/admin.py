f2f8fffdbc16f643ae59ecc37485c21df310de74
almost-good <marja.marinic@gmail.com>
2025-01-09 18:58:02 +0000

    update Task model registration to use django_summernote

--- tasker/admin.py
+++ tasker/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Task, Subtask
 
 
-# Model registration.
-admin.site.register(Task)
+# Model registration for: Task.
+@admin.register(Task)
+class TaskAdmin(SummernoteModelAdmin):
+    list_display = ('title')
+
+
+# Model registration for: Subtask.
 admin.site.register(Subtask)
