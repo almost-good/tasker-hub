b014f711dfe5c1c6cd9704eecc7f0285215242f0
almost-good <marja.marinic@gmail.com>
2025-01-11 21:00:47 +0000

    fix url path for tasks as it is conflicting with acc authentication

--- tasker/urls.py
+++ tasker/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('browse-tasks', views.BrowseTasksView.as_view(), name='browse-tasks'),
-    path('<str:username>/<slug:slug>/', views.task_detail_view, name='task-detail'),
+    path('tasker/<str:username>/<slug:slug>/', views.task_detail_view, name='task-detail'),
 ]
